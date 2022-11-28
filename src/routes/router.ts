8e231b1bbad3a4fedc451c499f4e5bd5461322bd
luisc68 <j.cerna@unadeca.net>
2022-11-27 23:17:55 -0600

    Alumno

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,19 +1,14 @@
 import { Application } from "express";
 import exampleRouter from "./example";
-<<<<<<< HEAD
 import alumnoRouter from "./alumno";
-=======
 import profesorRouter from "./profesor";
->>>>>>> main
+
 
 export default function router(app: Application): void {
   /**
    * Every source are specifed here
    */
   app.use("/", exampleRouter);
-<<<<<<< HEAD
   app.use("/alumno", alumnoRouter);
-=======
   app.use("/profesor", profesorRouter);
->>>>>>> main
 }
